4c8ae7d86053749a6751d478184f75564fe129db
Matt Ballert <nerdalert1984@fullsail.edu>
2012-10-19 00:43:52 -0400

    Added string manipulation.

--- js/Ballert_Matt_4.js
+++ js/Ballert_Matt_4.js
@@ -2,3 +2,15 @@
 // 10/10/2012
 // Project 4
 // Preparing For a New Journey In Coding
+
+// String Manipulation (Changing Separators)
+var abc = "a,b,c" + "," + "/"
+
+var separator = function ()
+	{
+	outputAbc = abc.charAt(0) + abc.charAt(6) + abc.charAt(2) + abc.charAt(6) + abc.charAt(4);
+	console.log(outputAbc);
+	return outputAbc
+	};
+
+var showOutput = separator()
