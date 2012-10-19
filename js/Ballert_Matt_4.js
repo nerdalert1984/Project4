1cdf82bf4be964033853d053b87ba95497145123
Matt Ballert <nerdalert1984@fullsail.edu>
2012-10-19 01:20:18 -0400

    Added Phone string and Title Case string (not working right).

--- js/Ballert_Matt_4.js
+++ js/Ballert_Matt_4.js
@@ -14,6 +14,37 @@ var separator = function () {
 
 var showOutput = separator()
 
+// String Manipulation (Phone Number)
+
+var phoneNumber = function (numbers) 
+	{
+	phoneNum = numbers.substring(0,3) + "-" + numbers.substring(3,6) + "-" + numbers.substring(6,10);
+	return phoneNum
+	};
+	
+var thisNumber = phoneNumber("5089811474")
+console.log(thisNumber)
+
+// String Manipulation (Title Case)
+
+var makeProper = "make me a proper title"
+
+var	splitProper = makeProper.split(" ");
+	
+	makeProper = makeProper.substring(0,1).toUpperCase() + makeProper.substring(1);
+
+var getProper = function (i) {
+		for (var i = 0; i < splitProper.length; i++) {
+		splitProp = splitProper[i].substring(0,1).toUpperCase() + splitProper[i].substring(1);
+	};
+return splitProp;
+};
+	
+var thisProp = getProper(0)
+
+console.log(makeProper)
+console.log(thisProp)
+
 // Numbers to Dollars
 
 var makeDollar = function (number) {
